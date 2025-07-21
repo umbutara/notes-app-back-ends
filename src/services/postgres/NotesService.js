b934fb11bf941d535a3775924a612b73389caee9
Milton Dapamudang <miltondapamudang@gmail.com>
2025-07-22 01:09:16 +0800

    user registration implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,7 +1,7 @@
-const { Pool } = require('pg');
-const { nanoid } = require('nanoid');
-const InvariantError = require('../../exceptions/InvariantError');
-const NotFoundError = require('../../exceptions/NotFoundError');
+  const { Pool } = require('pg');
+  const { nanoid } = require('nanoid');
+  const InvariantError = require('../../exceptions/InvariantError');
+  const NotFoundError = require('../../exceptions/NotFoundError');
 const { mapDBToModel } = require('../../utils');
 
 class NotesService {
